c08a37658169e4330985761f84273b0fc20ac623
Luiz Medina <luizmedina87@gmail.com>
2026-05-19 16:58:44 -0400

    add clock

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ from logger import log_state
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    # pygame setup
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
+    # game loop
     while True:
         log_state()
         for event in pygame.event.get():
-            screen.fill("black")
-            pygame.display.flip()
             if event.type == pygame.QUIT:
                 return
+        screen.fill("black")
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
