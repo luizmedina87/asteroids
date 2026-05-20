65d87d6ddcd9ede1491378d00e329e6bec5b629a
Luiz Medina <luizmedina87@gmail.com>
2026-05-20 11:25:50 -0400

    draw ship

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -17,6 +18,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        x = SCREEN_WIDTH / 2
+        y = SCREEN_HEIGHT / 2
+        player = Player(x, y)
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
