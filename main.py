2aad5263c16a5c5188891c9f822dce477276006a
Luiz Medina <luizmedina87@gmail.com>
2026-05-26 15:17:19 -0400

    fix shoot log

--- main.py
+++ main.py
@@ -19,10 +19,11 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
-    Shot.containers = (updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
     asteroidfield = AsteroidField()
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
